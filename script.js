b4654c3c5cc0b9ac92870b8149ec0272818184bc
TEDDI GUNA <teddyguna95@gmail.com>
2026-07-04 21:48:50 +0530

    Refactor script.js for theme and menu handling

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function() {
       submitBtn.disabled = true;
       
       try {
-        const response = await fetch('https://formspree.io/f/xdaryvzv', {
+        const response = await fetch('https://formspree.io/f/xwvdajwq', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
